9a0b9901fd1e2f142e766d75c68726c5d669bbfa
Masayuki Sunahara <tamanishi@gmail.com>
2023-06-01 00:05:00 +0900

    format

--- dump_google_calendar.py
+++ dump_google_calendar.py
@@ -10,7 +10,8 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 
 # If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+
 
 def main(args):
     """Shows basic usage of the Google Calendar API.
@@ -20,44 +21,65 @@ def main(args):
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
-        with open('token.json', 'w') as token:
+        with open("token.json", "w") as token:
             token.write(creds.to_json())
 
-    service = build('calendar', 'v3', credentials=creds)
+    service = build("calendar", "v3", credentials=creds)
 
     # Call the Calendar API
     min_date = datetime.datetime(int(args[1]), 1, 1, 0, 0, 0)
-    min_str = min_date.isoformat() + 'Z'
-    max_str = (min_date + relativedelta(years=1) - datetime.timedelta(seconds=1)).isoformat() + 'Z'
-    events_result = service.events().list(calendarId='primary', timeMin=min_str, timeMax=max_str,
-                                        maxResults=1000, singleEvents=True,
-                                        orderBy='startTime').execute()
-    events = events_result.get('items', [])
+    min_str = min_date.isoformat() + "Z"
+    max_str = (
+        min_date + relativedelta(years=1) - datetime.timedelta(seconds=1)
+    ).isoformat() + "Z"
+    events_result = (
+        service.events()
+        .list(
+            calendarId="primary",
+            timeMin=min_str,
+            timeMax=max_str,
+            maxResults=1000,
+            singleEvents=True,
+            orderBy="startTime",
+        )
+        .execute()
+    )
+    events = events_result.get("items", [])
 
     # # print(json.dumps(events))
-    print(json.dumps(list(map(lambda e: {
-        'summary': e.get('summary'),
-        'start': e.get('start').get('dateTime'),
-        'end': e.get('end').get('dateTime'),
-        'location': e.get('location'),
-        'description': e.get('description')}, events))))
+    print(
+        json.dumps(
+            list(
+                map(
+                    lambda e: {
+                        "summary": e.get("summary"),
+                        "start": e.get("start").get("dateTime"),
+                        "end": e.get("end").get("dateTime"),
+                        "location": e.get("location"),
+                        "description": e.get("description"),
+                    },
+                    events,
+                )
+            )
+        )
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     args = sys.argv
 
     if len(args) != 2:
-        print('Usage: python dump_google_calendar.py <year>')
+        print("Usage: python dump_google_calendar.py <year>")
         sys.exit(1)
 
     main(args)
